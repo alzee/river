2a3e2718335867dd24d2e06e3169ff2884621a67
Al Zee <z@alz.ee>
2024-02-22 12:40:53 +0800

    nth

---
+++ src/Controller/Admin/PatternCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Pattern;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class PatternCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Pattern::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}

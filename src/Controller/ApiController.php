0f96e408935d6614b93a188c26d464858ef0804c
Al Zee <z@alz.ee>
2024-02-23 10:35:28 +0800

    nth

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Service\Wx;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+
+#[Route('/api')]
+class ApiController extends AbstractController
+{
+    private $doctrine;
+    private $wx;
+
+    public function __construct(ManagerRegistry $doctrine, Wx $wx)
+    {
+        $this->doctrine = $doctrine;
+        $this->wx = $wx;
+    }
+    
+    #[Route(path: '/wxgetphone', name: 'api_wx_getphone', methods: ['POST'])]
+    public function wxLogin(Request $request)
+    {
+        $data = json_decode($request->getContent());
+        $code = $data->code;
+        $resp = $this->wx->getPhoneNumber($code);
+        return $this->json($resp);
+    }
+}

e0dfb1085ca0cfac87e74a00b6166e202d71494e
Al Zee <z@alz.ee>
2024-02-27 12:06:36 +0800

    nth

--- src/Controller/Admin/PatternSoilCrudController.php
+++ src/Controller/Admin/PatternSoilCrudController.php
@@ -9,6 +9,10 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 
 class PatternSoilCrudController extends AbstractCrudController
 {
@@ -17,18 +21,44 @@ class PatternSoilCrudController extends AbstractCrudController
         return Pattern::class;
     }
 
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->remove('index', 'new')
+            ->remove('index', 'delete')
+        ;
+    }
+    
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setPageTitle('index', 'Soil')
+            ->setPageTitle('edit', 'Soil')
+        ;
+    }
+
     public function configureFields(string $pageName): iterable
     {
         yield IdField::new('id')->onlyOnIndex();
-        yield TextField::new('name');
+        // yield FormField::addColumn(12);
+        yield TextField::new('SN')
+            ->setDisabled()
+            ->setColumns(4)
+        ;
+        yield TextField::new('name')
+            ->setDisabled()
+            ->setColumns(8)
+        ;
         // yield AssociationField::new('soils');
+     
         yield CollectionField::new('soils')
             ->useEntryCrudForm()
             ->renderExpanded()
             ->allowAdd(false)
             ->allowDelete(false)
             ->onlyWhenUpdating()
+            ->setColumns(12)
         ;
-        yield TextField::new('soil0.nianFenSha')->onlyOnIndex();
+        yield TextField::new('soil0.nianFenSha', 'nianFenSha')->onlyOnIndex();
     }
 }
--- src/Controller/Admin/SoilCrudController.php
+++ src/Controller/Admin/SoilCrudController.php
@@ -8,6 +8,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 
 class SoilCrudController extends AbstractCrudController
 {
@@ -18,8 +19,16 @@ class SoilCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        yield IdField::new('id')->onlyOnIndex();
-        yield AssociationField::new('pattern');
-        yield TextField::new('nianFenSha');
+        // yield AssociationField::new('pattern');
+        // yield FormField::addColumn(12);
+        yield TextField::new('nianFenSha')
+            ->setColumns(4)
+            ;
+        yield TextField::new('tuRangZhiDi')
+            ->setColumns(4)
+            ;
+        yield TextField::new('ganRongLiang')
+            ->setColumns(4)
+            ;
     }
 }
